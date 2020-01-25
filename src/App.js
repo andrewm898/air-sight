d02acc5ced530a445dbdd9b3af24922d75eea51e
Harrison McCarty <mccarth@purdue.edu>
2020-01-25 03:11:33 -0500

    UI fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import MapContainer from "./components/Map/GoogleMap";
+import GoogleMap from "./components/Map/GoogleMap";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <MapContainer></MapContainer>
+      <GoogleMap></GoogleMap>
     </div>
   );
 }
