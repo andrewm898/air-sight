ac4bb186e6238a5c9abc1fda9ee38d2407b236f3
Harrison McCarty <mccarth@purdue.edu>
2020-01-25 13:38:45 -0500

    Fixed bugs after the merge

--- src/components/Map/GoogleMap.js
+++ src/components/Map/GoogleMap.js
@@ -1,98 +1,11 @@
 import React, { Component } from "react";
+import { style, streetStyle } from "./GoogleMapStyles";
 import Switch from "react-switch";
 import "./GoogleMap.css";
 import { Map, Marker, GoogleApiWrapper, InfoWindow } from "google-maps-react";
 import db from "../../firebaseConfig";
 
 const auth = require("../../auth.json");
-
-const style = {
-  width: "100%",
-  height: "95.25vh"
-};
-const streetStyle = [
-  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-  {
-    elementType: "labels.text.stroke",
-    stylers: [{ color: "#242f3e" }]
-  },
-  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-  {
-    featureType: "administrative.locality",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }]
-  },
-  {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }]
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [{ color: "#263c3f" }]
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#6b9a76" }]
-  },
-  {
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [{ color: "#38414e" }]
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.stroke",
-    stylers: [{ color: "#212a37" }]
-  },
-  {
-    featureType: "road",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#9ca5b3" }]
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [{ color: "#746855" }]
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry.stroke",
-    stylers: [{ color: "#1f2835" }]
-  },
-  {
-    featureType: "road.highway",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#f3d19c" }]
-  },
-  {
-    featureType: "transit",
-    elementType: "geometry",
-    stylers: [{ color: "#2f3948" }]
-  },
-  {
-    featureType: "transit.station",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }]
-  },
-  {
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [{ color: "#17263c" }]
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#515c6d" }]
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.stroke",
-    stylers: [{ color: "#17263c" }]
-  }
-];
 
 export class GoogleMap extends Component {
   constructor() {
@@ -110,7 +23,6 @@ export class GoogleMap extends Component {
   }
 
   handleChange(checked) {
-    console.log(checked);
     this.setState({ checked });
   }
 
@@ -122,7 +34,6 @@ export class GoogleMap extends Component {
       activeMarker: marker,
       showingInfoWindow: true
     });
-    console.log(e.latLng.lat(), e.latLng.lng());
   };
 
   onMapClicked = (props, e) => {
@@ -143,7 +54,7 @@ export class GoogleMap extends Component {
       changes.forEach(change => {
         console.log(change.doc.data());
         if (change.type == "added") {
-          // renderCafe(change.doc);
+          console.log(change.doc.data());
           this.setState(
             (this.state.markers = this.state.markers.concat(
               <Marker
@@ -167,6 +78,34 @@ export class GoogleMap extends Component {
     });
   };
 
+  switchContainer = () => {
+    return (
+      <div>
+        <div className="switch-container">
+          <Switch
+            onChange={this.handleChange}
+            checked={this.state.checked}
+            checkedIcon={false}
+            uncheckedIcon={false}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  liveWindow = () => {
+    return (
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+      >
+        <div>
+          {"(" + this.state.x.toFixed(6) + ", " + this.state.y.toFixed(6) + ")"}
+        </div>
+      </InfoWindow>
+    );
+  };
+
   render() {
     return (
       <div>
@@ -185,27 +124,9 @@ export class GoogleMap extends Component {
             disableDefaultUI={true}
             mapType={"roadmap"}
           >
-            <div className="switch-container">
-              <Switch
-                onChange={this.handleChange}
-                checked={this.state.checked}
-                checkedIcon={false}
-                uncheckedIcon={false}
-              />
-            </div>
             {this.state.markers}
-            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-            >
-              <div>
-                {"(" +
-                  this.state.x.toFixed(6) +
-                  ", " +
-                  this.state.y.toFixed(6) +
-                  ")"}
-              </div>
-            </InfoWindow>
+            {this.switchContainer()}
+            {this.liveWindow()}
           </Map>
         )}
         {!this.state.checked && (
@@ -221,37 +142,9 @@ export class GoogleMap extends Component {
             disableDefaultUI={true}
             mapType={"satellite"}
           >
-            <div className="switch-container">
-              <Switch
-                onChange={this.handleChange}
-                checked={this.state.checked}
-                checkedIcon={false}
-                uncheckedIcon={false}
-              />
-            </div>
-            <Marker
-              name={"Corec"}
-              position={{ lat: 40.424, lng: -86.929 }}
-              onClick={this.onMarkerClick}
-              icon={{
-                url:
-                  "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/emojione/211/fire_1f525.png",
-                anchor: new window.google.maps.Point(32, 32),
-                scaledSize: new window.google.maps.Size(64, 64)
-              }}
-            />
-            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-            >
-              <div>
-                {"(" +
-                  this.state.x.toFixed(6) +
-                  ", " +
-                  this.state.y.toFixed(6) +
-                  ")"}
-              </div>
-            </InfoWindow>
+            {this.state.markers}
+            {this.switchContainer()}
+            {this.liveWindow()}
           </Map>
         )}
       </div>
